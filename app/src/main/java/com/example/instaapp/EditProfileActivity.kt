ba37ce0d513ef43db2961f71261b7b2788b91817
nopalodon <nflcoding@gmail.com>
2026-09-15 22:18:47 +0700

    process input, and then give it back to mainActi using setResult()

--- app/src/main/java/com/example/instaapp/EditProfileActivity.kt
+++ app/src/main/java/com/example/instaapp/EditProfileActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instaapp.databinding.ActivityEditProfileBinding
 
@@ -15,14 +16,35 @@ class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
 
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_BIO = "extra_bio"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val oldName = intent.getStringExtra(EXTRA_NAME)
+        val oldBio = intent.getStringExtra(EXTRA_BIO)
+
+        binding.editName.setText(oldName)
+        binding.editBio.setText(oldBio)
+
+        binding.btnSave.setOnClickListener {
+            val newName = binding.editName.text.toString()
+            val newBio = binding.editBio.text.toString()
+
+            val resultIntent = Intent().apply {
+                putExtra(EXTRA_NAME, newName)
+                putExtra(EXTRA_BIO, newBio)
+            }
+
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
         }
+
+
     }
 }
