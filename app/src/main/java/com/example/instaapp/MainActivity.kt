e3a3ae52d238cbc11cd55e2d445c26cbe3cd9e3e
nopalodon <nflcoding@gmail.com>
2026-09-15 22:44:28 +0700

    Implement edit profile

--- app/src/main/java/com/example/instaapp/MainActivity.kt
+++ app/src/main/java/com/example/instaapp/MainActivity.kt
@@ -5,16 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.instaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val editProfileLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            val updatedName = data?.getStringExtra(EditProfileActivity.EXTRA_NAME)
+            val updatedBio = data?.getStringExtra(EditProfileActivity.EXTRA_BIO)
+
+            // Update UI jika data tidak null
+            if (!updatedName.isNullOrEmpty()) {
+                binding.username.text = updatedName
+            }
+            if (!updatedBio.isNullOrEmpty()) {
+                binding.bio.text = updatedBio
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEditProfile.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java).apply {
+                putExtra(EditProfileActivity.EXTRA_NAME, binding.username.text.toString())
+                putExtra(EditProfileActivity.EXTRA_BIO, binding.bio.text.toString())
+            }
+            editProfileLauncher.launch(intent)
         }
     }
 }
